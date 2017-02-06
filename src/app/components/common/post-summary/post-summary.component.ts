3ad423ad68a3e8ae5f6928932f21b84d67b58fce
Jack Liddiard <jack.liddiard@iiincubator.com>
2017-02-06 20:43:02 +0000

    Removed snap scrolling

--- src/app/components/common/post-summary/post-summary.component.ts
+++ src/app/components/common/post-summary/post-summary.component.ts
@@ -1,6 +1,5 @@
-import {Component, OnInit, Input, AfterContentInit} from '@angular/core';
+import {Component, Input, AfterContentInit} from '@angular/core';
 import {Post} from '../../../models/post';
-import {MnFullpageService} from 'ng2-fullpage';
 
 @Component({
   selector: 'app-post-summary',
@@ -11,10 +10,9 @@ export class PostSummaryComponent implements AfterContentInit {
 
   @Input() post: Post;
 
-  constructor(private fullpageService: MnFullpageService) {}
+  constructor() {}
 
   ngAfterContentInit() {
-    this.fullpageService.reBuild();
   }
 
 }
