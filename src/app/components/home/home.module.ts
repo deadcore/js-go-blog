0e1a9a5c5fc37e4d2338e60282e501a04b10f52b
Jack Liddiard <jack.liddiard@iiincubator.com>
2017-02-07 08:37:56 +0000

    Removed full page Added full height to content on admin

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -2,7 +2,6 @@ import {CommonModule} from '@angular/common';
 import {HomeComponent} from './home.component';
 import {NgModule} from '@angular/core';
 import {SharedModule} from '../common/shared.module';
-import {MnFullpageDirective, MnFullpageService} from 'ng2-fullpage';
 import {PostListComponent} from './post-list/post-list.component';
 
 @NgModule({
